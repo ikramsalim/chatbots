1df21fd16a781e3aa4e3e797123657bdb7281049
Ikram <ikramomar998@gmail.com>
2020-03-15 08:18:45 -0600

    uses re.sub() to map "me" and "my" to "you" and "your" (and vice versa) in a string.

--- chatbots-101/echobot-I.py
+++ chatbots-101/echobot-I.py
@@ -122,3 +122,29 @@ def match_rule(rules, message):
 
 # Test match_rule
 print(match_rule(rules, "do you remember your last birthday"))
+
+"""
+ELIZA : PRONOUNS
+"""
+# Define replace_pronouns()
+def replace_pronouns(message):
+
+    message = message.lower()
+    if 'me' in message:
+        # Replace 'me' with 'you'
+        return re.sub('me','you',message)
+    if 'my' in message:
+        # Replace 'my' with 'your'
+        return re.sub('my','your',message)
+    if 'your' in message:
+        # Replace 'your' with 'my'
+        return re.sub('your','my',message)
+    if 'you' in message:
+        # Replace 'you' with 'me'
+        return re.sub('you','me',message)
+
+    return message
+
+print(replace_pronouns("my last birthday"))
+print(replace_pronouns("when you went to Florida"))
+print(replace_pronouns("I had my own castle"))

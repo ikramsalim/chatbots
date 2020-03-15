c4c9c5c239442f641d35625c335d8e438b75ccff
Ikram <ikramomar998@gmail.com>
2020-03-15 07:40:40 -0600

    EchoBot just responds by replying with the same message it receives.

---
+++ chatbots-101/echobot-I.py
@@ -0,0 +1,15 @@
+"""
+EchoBot just responds by replying with the same message it receives.
+"""
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+# Define a function that responds to a user's message: respond
+def respond(message):
+    # Concatenate the user's message to the end of a standard bot respone
+    bot_message = "I can hear you! You said: " + message
+    # Return the result
+    return bot_message
+
+# Test function
+print(respond("hello!"))

5a110551343560b650035298944340cef7ee3033
Ikram <ikramomar998@gmail.com>
2020-03-15 07:44:32 -0600

    define a function called send_message() with a single parameter message which logs the message and the bot's response.

--- chatbots-101/echobot-I.py
+++ chatbots-101/echobot-I.py
@@ -13,3 +13,21 @@ def respond(message):
 
 # Test function
 print(respond("hello!"))
+
+""" ECHOBOT-II"""
+
+# Create templates
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+# Define a function that sends a message to the bot: send_message
+def send_message(message):
+    # Print user_template including the user_message
+    print(user_template.format(message))
+    # Get the bot's response to the message
+    response = respond(message)
+    # Print the bot template including the bot's response.
+    print(bot_template.format(response))
+
+# Send a message to the bot
+send_message("hello")
